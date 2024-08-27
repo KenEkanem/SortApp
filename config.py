c00b056d9810068dc219ad3821ad8ed3f885583a
KenEkanem <kennedyekanem@gmail.com>
2024-08-27 16:49:31 +0100

    removed smtp from config, using .env

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 SMTP_SERVER = 'smtp.your-email.com'
-SMTP_PORT = 465
-SMTP_USER = 'your-email@example.com'
-SMTP_PASSWORD = 'your-password'
+# SMTP_PORT = 465
+# SMTP_USER = 'your-email@example.com'
+# SMTP_PASSWORD = 'your-password'
 CSV_FILE = 'data/guests.csv'
 TEMPLATE_FILE = 'templates/access_card_template.png'
 QR_CODE_DIR = 'qr_codes/'
