fa6e256fdf44866772713f789e04ed2ba9e147b6
KenEkanem <kennedyekanem@gmail.com>
2024-08-28 12:17:18 +0100

    updated barcode id

--- src/sheets_updater.py
+++ src/sheets_updater.py
@@ -3,11 +3,11 @@ import requests
 # URL for Google Apps Script
 GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwpYPdv8pf_4nLaAg7bP1IqHsTIETdJwBF8hRVy2micuD9EyKm9m0chcODUofWFGtDWJA/exec"
 
-def update_barcode_in_sheet(email, barcode):
+def update_barcode_in_sheet(email, user_id):
     # Define the parameters for the POST request
     params = {
         'email': email,
-        'barcode': barcode
+        'unique_id': user_id
     }
     
     try:
