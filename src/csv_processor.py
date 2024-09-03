ab198a7c95f6013a218103cfe4a9f13610a6bfcf
KenEkanem <kennedyekanem@gmail.com>
2024-09-03 18:40:02 +0100

    update csv processor

--- src/csv_processor.py
+++ src/csv_processor.py
@@ -3,9 +3,8 @@ import pandas as pd
 def load_guest_data(csv_file):
     try:
         # Load CSV and keep only the relevant columns
-        guests = pd.read_csv(csv_file, usecols=['name', 'email', 'unique_id', 'barcode'])
+        guests = pd.read_csv(csv_file, usecols=['name', 'email', 'unique_id', 'xn', 'barcode'])
         return guests
     except Exception as e:
         print(f"Error loading CSV file: {e}")
         return None
-
